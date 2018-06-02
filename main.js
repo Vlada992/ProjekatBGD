1381388d52ba8bace5c671555614e80ac91ce418
Vladan92 <vladanng@gmail.com>
2018-06-03 01:04:11 +0200

    BgdProjekat improvements/news/flex

--- main.js
+++ main.js
@@ -1,46 +1,66 @@
 
-function show(shown, hidden, hidden1, hidden2){ 
-  document.getElementById(shown).style.display    = 'block';
+function show(shown, hidden, hidden1, hidden2, hidden3){ 
+  document.getElementById(shown).style.display  = 'block';
   if(shown == 'Page1'){
   document.querySelector('#navA5').style.backgroundColor = "#dcdccb"
   document.querySelector('#navA3').style.backgroundColor = '';
   document.querySelector('#navA1').style.backgroundColor = '';
   document.querySelector('#navA4').style.backgroundColor = '';
+  document.querySelector('#navA2').style.backgroundColor = '';
   document.querySelector('#navA5').style.borderBottom = '3px solid #8CB240'
   document.querySelector('#navA3').style.borderBottom = ''
   document.querySelector('#navA1').style.borderBottom = ''   
-  document.querySelector('#navA4').style.borderBottom = ''   
-} else if(shown == 'Page2'){
+  document.querySelector('#navA4').style.borderBottom = ''
+  document.querySelector('#navA2').style.borderBottom = '';
+}else if(shown == 'Page2'){
   document.querySelector('#navA5').style.backgroundColor = ""
   document.querySelector('#navA3').style.backgroundColor = '#dcdccb';
   document.querySelector('#navA1').style.backgroundColor = '';
   document.querySelector('#navA4').style.backgroundColor = '';
+  document.querySelector('#navA2').style.backgroundColor = '';
   document.querySelector('#navA3').style.borderBottom = '3px solid #8CB240'
-   document.querySelector('#navA4').style.borderBottom = ''
+  document.querySelector('#navA4').style.borderBottom = ''
   document.querySelector('#navA1').style.borderBottom = ''   
-  document.querySelector('#navA5').style.borderBottom = '' 
-} else if(shown == 'Page3'){
+  document.querySelector('#navA5').style.borderBottom = ''
+  document.querySelector('#navA2').style.borderBottom = '';
+}else if(shown == 'Page3'){
     document.querySelector('#navA3').style.backgroundColor = '';
     document.querySelector('#navA5').style.backgroundColor = '';
     document.querySelector('#navA4').style.backgroundColor = '#dcdccb';
     document.querySelector('#navA1').style.backgroundColor = '';
+    document.querySelector('#navA2').style.backgroundColor = '';
     document.querySelector('#navA4').style.borderBottom = '3px solid #8CB240'
     document.querySelector('#navA3').style.borderBottom = ''
     document.querySelector('#navA1').style.borderBottom = ''   
     document.querySelector('#navA5').style.borderBottom = '' 
-} else if(shown == 'Page4'){
+    document.querySelector('#navA2').style.borderBottom = '';
+}else if(shown == 'Page4'){
     document.querySelector('#navA4').style.backgroundColor = '';
     document.querySelector('#navA3').style.backgroundColor = '';
     document.querySelector('#navA5').style.backgroundColor = '';
     document.querySelector('#navA1').style.backgroundColor = '#dcdccb'
+    document.querySelector('#navA2').style.backgroundColor = '';
     document.querySelector('#navA1').style.borderBottom = '3px solid #8CB240'
     document.querySelector('#navA3').style.borderBottom = ''
     document.querySelector('#navA5').style.borderBottom = ''   
     document.querySelector('#navA4').style.borderBottom = '' 
+    document.querySelector('#navA2').style.borderBottom = '';
+}else if(shown == 'Page5'){
+    document.querySelector('#navA4').style.backgroundColor = '';
+    document.querySelector('#navA3').style.backgroundColor = '';
+    document.querySelector('#navA5').style.backgroundColor = '';
+    document.querySelector('#navA1').style.backgroundColor = ''
+    document.querySelector('#navA2').style.backgroundColor = '#dcdccb';
+    document.querySelector('#navA1').style.borderBottom = ''
+    document.querySelector('#navA3').style.borderBottom = ''
+    document.querySelector('#navA5').style.borderBottom = ''   
+    document.querySelector('#navA4').style.borderBottom = '' 
+    document.querySelector('#navA2').style.borderBottom = '3px solid #8CB240';
 }
   document.getElementById(hidden).style.display   ='none';
   document.getElementById(hidden1).style.display  ='none';
   document.getElementById(hidden2).style.display  ='none';
+  document.getElementById(hidden3).style.display  ='none';
   return false;
 }
 
@@ -69,28 +89,27 @@ function onMapClick(e){
     console.log("You clicked the map at " + e.latlng);
 };
 mymap.on('click', onMapClick);
-var marker =  L.marker([44.816459, 20.460835]).addTo(mymap);
-var marker1 = L.marker([44.831891, 20.435944]).addTo(mymap)
-var marker2 = L.marker([44.822882, 20.449848]).addTo(mymap)
-var marker3 = L.marker([44.818986, 20.294838]).addTo(mymap)
-var marker4 = L.marker([44.814906, 20.448732]).addTo(mymap)
-var marker5 = L.marker([44.797246, 20.42573]).addTo(mymap)
-var marker6 = L.marker([44.799846, 20.436177]).addTo(mymap)
-var marker7 = L.marker([44.790364, 20.409937]).addTo(mymap)
-var marker8 = L.marker([44.802545, 20.440793]).addTo(mymap)
-var marker9 = L.marker([44.810918, 20.44767]).addTo(mymap)
+var marker   = L.marker([44.816459, 20.460835]).addTo(mymap);
+var marker1  = L.marker([44.831891, 20.435944]).addTo(mymap)
+var marker2  = L.marker([44.822882, 20.449848]).addTo(mymap)
+var marker3  = L.marker([44.818986, 20.294838]).addTo(mymap)
+var marker4  = L.marker([44.814906, 20.448732]).addTo(mymap)
+var marker5  = L.marker([44.797246, 20.42573]).addTo(mymap)
+var marker6  = L.marker([44.799846, 20.436177]).addTo(mymap)
+var marker7  = L.marker([44.790364, 20.409937]).addTo(mymap)
+var marker8  = L.marker([44.802545, 20.440793]).addTo(mymap)
+var marker9  = L.marker([44.810918, 20.44767]).addTo(mymap)
 var marker10 = L.marker([44.828376, 20.491916]).addTo(mymap)
 var marker11 = L.marker([44.801346, 20.439154]).addTo(mymap)
 var marker12 = L.marker([44.8083, 20.488526]).addTo(mymap)
 var marker13 = L.marker([44.741856, 20.319353]).addTo(mymap)
 var marker14 = L.marker([44.864379, 20.381114]).addTo(mymap)
-var circle = L.circle([44.7950478,20.4394765,17.71], {
+var circle   = L.circle([44.7950478,20.4394765,17.71], {
     color: '#8CB240',
     fillColor: '#8CB240',
     fillOpacity: 0.3,
     radius: 8500
 }).addTo(mymap);
-
 marker.bindPopup("<br><b><a href='https://en.wikipedia.org/wiki/Republic_Square_(Belgrade)' target='_blank'>Republic Square</a></b>")
 marker1.bindPopup('<b><a href="https://en.wikipedia.org/wiki/Great_War_Island" target="_blank">Great War Island</a></b>')
 marker2.bindPopup("<b><a href='https://en.wikipedia.org/wiki/Belgrade_Fortress' target='_blank'>Belgrade Fortress</a></b>")
@@ -107,7 +126,6 @@ marker12.bindPopup("<b><a href='https://en.wikipedia.org/wiki/Belgrade_New_Cemet
 marker13.bindPopup("<b><a href='https://en.wikipedia.org/wiki/Ostru%C5%BEnica_Bridge' target='_blank'>Ostružnica Bridge</a></b>")
 marker14.bindPopup("<b><a href='https://en.wikipedia.org/wiki/Pupin_Bridge' target='_blank'>Pupin Bridge</a></b>")
 
-
     var myHeaders = new Headers();
     var myInit = { 
                   method: 'GET',
@@ -117,16 +135,20 @@ marker14.bindPopup("<b><a href='https://en.wikipedia.org/wiki/Pupin_Bridge' targ
                  };
     var myRequest = new Request('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20url%20in%20(%27http%3A%2F%2Fwww.beograd.rs%2Flat%2Frss%2F%27)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys');
     fetch(myRequest, myInit)
-    .then(dataRSS => {
-      return dataRSS.json()
+    .then(beogradNewsRS => {
+      return beogradNewsRS.json()  
     })
-    .then(dataR => {
-      console.log('BG sajt RSS feed:',dataR);
-        for(var i = 0; i < dataR.query.count; i++){ 
+    .then(beogradRS => {
+      console.log('BG sajt RSS feed:',beogradRS);
+        for(var i = 0; i < beogradRS.query.count; i++){ 
+                 var dataR = beogradRS;
                  var storeTitle = dataR.query.results.item[i].title;
                  var storeLink = dataR.query.results.item[i].link;
-                 var storeText = dataR.query.results.item[i].description;                
+                 var storeText = dataR.query.results.item[i].description;
                  var storeDate = dataR.query.results.item[i].pubDate;
+                 var momDate = storeDate.split('.').reverse();
+                 takeWeek = localStorage.getItem('dayW').split(',');
+                 var dow = takeWeek[moment(momDate.join('-').slice(1)).day()]  /*moment fine code*/
                  var storeImageUse = storeText.match(/"http[^\s]+ /);
                  var myImage = storeImageUse[0];
                  var myFinImage = myImage.charAt(myImage.length -4).toLowerCase() != 'p' ? 'images/images.png' :  myImage;
@@ -136,19 +158,19 @@ marker14.bindPopup("<b><a href='https://en.wikipedia.org/wiki/Pupin_Bridge' targ
                 $('#carousel-example-generic').carousel({
                 interval: 10000
                 });
-                 document.getElementsByClassName('item')[i].children[0].innerHTML = `<h4 id =titleId1> <a id='titleId1' href=${storeHref} target = '_blank'> ${storeTitle}  </h4> </a>` + `<img title = "${storeTitle}" src = ${myFinImage}  id =imgId1>`   +  "<p id = partTxt>"  +  `<span id =dateId1>  ${storeDate} </span>` +  storeTextPart +  `<a id =aHrefId1 href =${storeHref} target = blank>&nbsp;...&nbsp;Detailed<a/>`     +  "</p>"               
+                 document.getElementsByClassName('item')[i].children[0].innerHTML = ` <h4 id =titleId1> <span title='Belgrade news from beograd.rs'><i  id='titleIcnId1' class="fa fa-newspaper-o"></i></span>
+                  <a id='titleId1' href=${storeHref} target = '_blank'> ${storeTitle}  </h4> </a>` + `<img title = "${storeTitle}" src = ${myFinImage}  id =imgId1>` 
+                   +  "<p id = partTxt>"  +  `<span id =dateId1>  ${dow}, ${storeDate} </span>` +  storeTextPart +  `<a id =aHrefId1 href =${storeHref} target = blank>&nbsp;...&nbsp;Detailed<a/>`     +  "</p>"               
+                 document.getElementById('hitPlease1').style.display = 'block';
        }
     })
-    .catch(error =>{
-         console.log(error);
-    })
     fetch('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20url%20in%20(%27https%3A%2F%2Fwww.blic.rs%2Frss%2FVesti%2FBeograd%27)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys')        
-        .then((blicFeed)=>{
+        .then((blicFeed) => {
             return blicFeed.json();
         })
-        .then((blicRSS)=>{
+        .then((blicRSS) => {
             console.log("BLIC FEED:", blicRSS);
-            for(let i = 0; i < 20; i++){
+            for(let i = 0; i <= 41; i++){
                 var storeTitle1   =  blicRSS.query.results.item[i].title;
                 var storeLink1    =  blicRSS.query.results.item[i].link;
                 var storeText1    =   '&#x2014;' +  '&nbsp;' + blicRSS.query.results.item[i].description;
@@ -158,14 +180,16 @@ marker14.bindPopup("<b><a href='https://en.wikipedia.org/wiki/Pupin_Bridge' targ
                 var storeHref1    =  String(storeLink1);
                $('#myCarousel').carousel({
                 interval: 5000
-                })
-               document.getElementsByClassName('itemId2')[i].children[0].innerHTML = `&nbsp;&nbsp;<h4 id =titleId1> <a id ='titleId1' href = ${storeHref1} target = '_blank'> ${storeTitle1} &nbsp;</a></h4><br> &nbsp;&nbsp;&nbsp;&nbsp; <p id = 'partTxt1'>  <span id =dateId1> ${storeDatePart1}   </span> <span id='dateIdSl2'>&nbsp; ${storeDatePart2}  </span>${storeText1}<a id ='titleId1' href = ${storeHref1} target = '_blank'><i id ='glyId2' class="fa fa-external-link"></i></a></p> ` 
+                })  
+              document.getElementsByClassName('itemId2')[i].children[0].innerHTML = `&nbsp;&nbsp;<h4 id =titleId1><span><i title='Belgrade news from blic.rs' id='titleIcnId2' class="fas fa-newspaper"></i></span>
+             <a id ='titleId1' href = ${storeHref1} target = '_blank'>
+            ${storeTitle1} &nbsp;</a></h4><br>
+               &nbsp;&nbsp;&nbsp;&nbsp; <p id = 'partTxt1'>  <span id =dateId1> ${storeDatePart1}   </span> <span id='dateIdSl2'>&nbsp; ${storeDatePart2}  </span>${storeText1}<a id ='titleId1' href = ${storeHref1} target = '_blank'>
+               <i id ='glyId2' class="fa fa-external-link"></i></a></p> ` 
+              document.getElementById('fourSquareId').style.display = 'block';
             }
         })
-        .catch(error => {
-            console.log('Here is catched error:', error);
-        })
-
+  
       
 
       
--- seeWhat.js
+++ seeWhat.js
@@ -1,6 +1,4 @@
 
-
-
 function invokSkver(visible, notVisb1, notVisb2, notVisb3){
   document.getElementById(visible).style.display = 'block';
   document.getElementById(notVisb1).style.display = 'none';
@@ -13,10 +11,9 @@ fetch('https://api.foursquare.com/v2/venues/search?ll=44.8099375,20.4494431&cate
 .then( xVenAll => {
   return xVenAll.json();
 })
-.then( venueY => {
-    var allVenueX  = venueY.response.venues;
-    var eachVenId;
-    for(let i = 0; i < 50; i++){
+.then( foodY => {
+    var allVenueX  = foodY.response.venues;
+    for(let i = 0; i <= 41; i++){
     var storeImageFSQ = allVenueX[i].categories[0].icon.prefix + 64 +allVenueX[i].categories[0].icon.suffix;
     var defImgS       = localStorage.getItem('eto3');
     var storeCateg    = allVenueX[i].categories[0].name;
@@ -28,13 +25,18 @@ fetch('https://api.foursquare.com/v2/venues/search?ll=44.8099375,20.4494431&cate
     var storeAddr2    = allVenueX[i].location.formattedAddress[2] != undefined ?  `&nbsp; ${allVenueX[i].location.formattedAddress[2]}` : '&nbsp;  Serbia';
     var  number       = allVenueX[i].contact.formattedPhone != undefined ?  ''+`${allVenueX[i].contact.formattedPhone}` : `<span id ='ifNoNumId'>No number info</span>`;
     var takeLatLon    =  'https://www.google.com/maps/search/?api=1&query=' + allVenueX[i].location.lat + ',' + allVenueX[i].location.lng
-    document.getElementsByClassName('item4')[i].children[0].innerHTML =   ` <h3 title = ${allVenueX[i].categories[0].name} id = 'titleFSQ1'> ${allVenueX[i].name} &nbsp;&nbsp;&nbsp; <img id = 'imgFSQ1' src =${storeImageFSQ} /> <img id='defImg1' src=${defImgS}></h3>  <div id ='fsqTextDiv'> <p id ='adress4sq'> <i title='Location of place' class="fa fa-map-signs"></i> <a id='latLonForSq' href=${takeLatLon} target=_blank title='Click to get there'>&nbsp;&nbsp;&nbsp;&nbsp;${storeAddress}</a> <br> <span title='Explanation, nearby streets' id='crossId'>&nbsp;&#9580; </span> &nbsp;&nbsp;&nbsp; ${strCross} <br> <i title='In which city?'  class="fa fa-address-card-o styleFaCard"></i> &nbsp;&nbsp; ${storeAddr1} ${storeAddr2}<br> <span id ='boldNumId'><span title='Phone number' id ='phone4SqId'>&#9742;</span> &nbsp;&nbsp;&nbsp; ${number}</span></p><img title='Internet adress' id ='style4ImgUrl' src ='https://image.flaticon.com/icons/svg/109/109476.svg' style="width:26px;height:28px;"/><p>&nbsp;<a id = 'sq4UrlTxt' href = ${storeUrl} target = "_blank">${prtUrl}...</a></p><p id='showOnSm'>Visit Site<p></div><p id ='categ4sqId'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Categories: <span id='sq4StoreCat'>${storeCateg}</span></p>`;
+    
+    document.getElementsByClassName('item4')[i].children[0].innerHTML =   ` <h3 title = ${allVenueX[i].categories[0].name} id = 'titleFSQ1'> ${allVenueX[i].name} &nbsp;&nbsp;&nbsp; 
+    <img id = 'imgFSQ1' src =${storeImageFSQ} /> <img id='defImg1' src=${defImgS}></h3>  <div id ='fsqTextDiv'> <p id ='adress4sq'> <i title='Location of place' class="fa fa-map-signs"></i>
+     <a id='latLonForSq' href=${takeLatLon} target=_blank title='Click to get there'>
+    &nbsp;&nbsp;&nbsp;&nbsp;${storeAddress}</a> <br> <span title='Explanation, nearby streets' id='crossId'>&nbsp;&#9580; </span> &nbsp;&nbsp;&nbsp; ${strCross} <br> 
+    <i title='In which city?'  class="fa fa-address-card-o styleFaCard"></i> &nbsp;&nbsp; ${storeAddr1} ${storeAddr2}<br> <span id ='boldNumId'><span title='Phone number' id ='phone4SqId'>&#9742;</span>
+     &nbsp;&nbsp;&nbsp; ${number}</span></p><img title='Internet adress' id ='style4ImgUrl' src ='https://image.flaticon.com/icons/svg/109/109476.svg' style="width:26px;height:28px;"/><p>&nbsp;
+     <a id = 'sq4UrlTxt' href = ${storeUrl} target = "_blank">${prtUrl}...</a></p><p id='showOnSm'>Visit Site<p></div><p id ='categ4sqId'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+     Categories: <span id='sq4StoreCat'>${storeCateg}</span></p>`;
     }
 })
-.catch( error => {
-  console.log('sakriven err:', error);
-})
-.then(() =>{
+.then(() => {
   $.ajax({
       url: 'http://api.eventful.com/json/events/search?app_key=RWcbt5k294VSHHmJ&l=Belgrade&date=Future&image_sizes=block100,medium,dropshadow250&page_size=50&within=30',
       accepts:{accepts: "application/json"},
@@ -43,7 +45,21 @@ fetch('https://api.foursquare.com/v2/venues/search?ll=44.8099375,20.4494431&cate
       var takeEvent = eventJson.events.event;
        let weekday =  localStorage.getItem('dayW').split(","); 
        let month   =  localStorage.getItem('monthS').split(",");
-        for(let l = 0; l < takeEvent.length; l++){
+       console.log('eventfull API:', eventJson)
+       var imageArr = [];
+       var iterArr  = [];
+
+       for(let l = 0; l <= 20; l++){
+
+         var takeImg   = takeEvent[l].image != null ?  String(takeEvent[l].image.medium.url) : 'no image'
+           if(takeEvent[l].image != null && takeEvent[l].image.medium.url.startsWith('h')) {
+            imageArr.push(takeEvent[l].image.medium.url);
+            iterArr.push(l);
+          } else if(takeEvent[l].image != null && takeEvent[l].image.medium.url.startsWith('/')) {
+            imageArr.push('http:'+ takeEvent[l].image.medium.url)
+            iterArr.push(l)
+          }
+
         var cityName  = takeEvent[l].city_name;
         var countName = takeEvent[l].country_name;
         var strTime   = takeEvent[l].start_time;
@@ -66,8 +82,22 @@ fetch('https://api.foursquare.com/v2/venues/search?ll=44.8099375,20.4494431&cate
         var evnLoc    = takeEvent[l].olson_path.slice(7);
         var takeDesc  =  takeEvent[l].description != undefined ? takeEvent[l].description.slice(0, 38) : 'No description'
         var takeLL    =  takeEvent[l].latitude.slice(takeEvent[l].latitude.length -2) != 86 ?  'https://www.google.com/maps/search/?api=1&query=' + takeEvent[l].latitude + ',' + takeEvent[l].longitude : '';
-        var firstT = document.getElementsByClassName('item1');
-        firstT[l].children[1].innerHTML = `<p id='evTitl1'>${evTitle}</p>&nbsp;&nbsp; <p id=styleHr1></p><br><p id=monEvF> <img title='Date and time' src="images/calendar-with-a-clock-time-tools.png"/> &nbsp;  <span id='monDayId'>${monIs} ${dayIs}</span>, &nbsp;${yrIs} &nbsp; <span id =dashIdEvn>&minus;</span>&nbsp;  ${dayName}&nbsp;&nbsp;&nbsp;&nbsp; <i class="fa fa-long-arrow-right faIcn1"></i>  &nbsp;<span id=opnTmEvn>${opnTime}  &nbsp;&nbsp; </p>  <p id=dNameEvn> <img title='City' src="images/skyline.png"/> &nbsp;<span id=evnLocF>${evnLoc},  ${countName}</span></span></p> <p id='locEvnIc'><img title='Venue location'  src ="images/placeholderNewLoc.png"/> &nbsp;<a id='venUrlId' title='More info about venue' href= ${venUrl} target="_blank">  <span id='venNmEvn'>${venName}</span> </a></p> <p id='addrIcnEnv'><img  title='Where is it?' src="images/signalStreet.png"/> &nbsp;  <a id ='latLonHrefEv' href=${takeLL} target='_blank' title='See this place on the map'>${venAdrr}</a></p> <p id='linkEnvId'><img title= 'Event url' src="images/http.png"/> &nbsp; <a id='venEvnUrl'  title='Visit eventfull.com for more info'  href= ${evUrl} target = "_blank">${eveUrlPrt}...</a> </p><p id="descIdEv"><i title='Venue name' id ='icnPDesc' class="fa fa-paragraph"></i> &nbsp;&nbsp;&nbsp;&nbsp;<a id='descPrtId' href =${evUrl} target=_blank>${takeDesc}...</a></p>`
+        
+        document.getElementsByClassName('item1')[l].children[1].innerHTML = ` <p id='evTitl1'>${evTitle}</p>&nbsp;&nbsp; <p id=styleHr1></p><br>  <div class='divEvnAll' class='container'> <p id=monEvF>
+         <img title='Date and time' src="images/calendar-with-a-clock-time-tools.png"/> &nbsp;  <span id='monDayId'>${monIs} ${dayIs}</span>, &nbsp;${yrIs} &nbsp; <span id =dashIdEvn>&minus;</span>&nbsp;  
+        ${dayName}&nbsp;&nbsp;&nbsp;&nbsp; <i class="fa fa-long-arrow-right faIcn1"></i>  &nbsp;<span id=opnTmEvn>${opnTime}  &nbsp;&nbsp; </p>  <p id=dNameEvn> <img title='City' src="images/skyline.png"/>
+         &nbsp;<span id=evnLocF>${evnLoc},  ${countName} &nbsp; <img src='images/favicon-32x32.png'/></span></span></p> <p id='locEvnIc'><img title='Venue location'  src ="images/placeholderNewLoc.png"/>
+         &nbsp;<a id='venUrlId' title='More info about venue' href= ${venUrl} target="_blank">  <span id='venNmEvn'>${venName}</span> </a></p> <p id='addrIcnEnv'>
+         <img  title='Where is it?' src="images/signalStreet.png"/> &nbsp;  <a id ='latLonHrefEv' href=${takeLL} target='_blank' title='See this place on the map'>${venAdrr}</a></p>
+          <p id='linkEnvId'><img title= 'Event url' src="images/http.png"/> &nbsp; <a id='venEvnUrl'  title='Visit eventfull.com for more info'  href= ${evUrl} target = "_blank">${eveUrlPrt}...</a>
+         </p><p id="descIdEv"><i title='Venue name' id ='icnPDesc' class="fa fa-paragraph"></i> &nbsp;&nbsp;&nbsp;&nbsp;<a id='descPrtId' href =${evUrl} target=_blank>${takeDesc}...</a></p>   <img id='imgDrop250  src= ${takeImg}/> </div>`
+        }
+
+        console.log('moj array:', imageArr)
+        console.log('iteracije su:', iterArr)
+
+        for(let xx = 0; xx < imageArr.length; xx++ ){
+          document.getElementsByClassName('item1')[iterArr[xx]].children[1].innerHTML +=`<img id='picForEvent' src =${imageArr[xx]}/>`
         }
     }
  }) 
@@ -79,7 +109,7 @@ fetch('https://api.foursquare.com/v2/venues/search?ll=44.8099375,20.4494431&cate
  })
  .then(hotelS => {
   let allVenueX = hotelS.response.venues;
-  for(let i = 0; i < 50; i++){
+  for(let i = 0; i <=40; i++){
     var storeImageFSQ = allVenueX[i].categories[0].icon.prefix + 64 + allVenueX[i].categories[0].icon.suffix;
     var defImgS       = localStorage.getItem('eto1');
     var storeCateg    = allVenueX[i].categories[0].name;
@@ -91,25 +121,18 @@ fetch('https://api.foursquare.com/v2/venues/search?ll=44.8099375,20.4494431&cate
     var storeAddr2    = allVenueX[i].location.formattedAddress[2] != undefined ?  `&nbsp; ${allVenueX[i].location.formattedAddress[2]}` : '&nbsp;  Serbia';
     var  number       = allVenueX[i].contact.formattedPhone != undefined ?  ''+`${allVenueX[i].contact.formattedPhone}` : `<span id ='ifNoNumId'>No number info</span>`;
     var takeLatLon    =  'https://www.google.com/maps/search/?api=1&query=' + allVenueX[i].location.lat + ',' + allVenueX[i].location.lng
-
     document.getElementsByClassName('item2')[i].children[0].innerHTML =   ` <h3 title = ${allVenueX[i].categories[0].name} id = 'titleFSQ1'> ${allVenueX[i].name} &nbsp;&nbsp;&nbsp; <img id = 'imgFSQ1' src =${storeImageFSQ} /> <img id='defImg2' src=${defImgS}></h3>  <div id ='fsqTextDiv'> <p id ='adress4sq'> <i title='Location of place' class="fa fa-map-signs"></i> <a id='latLonForSq' href=${takeLatLon} target=_blank title='Click to get there'>&nbsp;&nbsp;&nbsp;&nbsp;${storeAddress}</a> <br> <span title='Explanation, nearby streets' id='crossId'>&nbsp;&#9580; </span> &nbsp;&nbsp;&nbsp; ${strCross} <br> <i title='In which city?'  class="fa fa-address-card-o styleFaCard"></i> &nbsp;&nbsp; ${storeAddr1} ${storeAddr2}<br> <span id ='boldNumId'><span title='Phone number' id ='phone4SqId'>&#9742;</span> &nbsp;&nbsp;&nbsp; ${number}</span></p><img title='Internet adress' id ='style4ImgUrl' src ='https://image.flaticon.com/icons/svg/109/109476.svg' style="width:26px;height:28px;"/><p>&nbsp;<a id = 'sq4UrlTxt' href = ${storeUrl} target = "_blank">${prtUrl}...</a></p><p id='showOnSm'>Visit Site<p></div><p id ='categ4sqId'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Categories: <span id='sq4StoreCat'>${storeCateg}</span></p>`;
     }
- })
- .catch((err)=> {
-   console.log(err)
  })
  fetch('https://api.foursquare.com/v2/venues/search?ll=44.8099375,20.4494431&categoryId=4d4b7105d754a06378d81259&&limit=50&client_id=SYQLZ1DXBSZYMCXG3QUGBBHDRM23YDDLO5SAZCALXMFUR3VS&client_secret=HHFBNGSRMFOUAFYQCTVMR1FK4HR4GZL5LO0T0BYGQVFUHSW0&v=20180130')
  .then(shopS => {
   return shopS.json()
 })
- .then(storeShops => {
+.then(storeShops =>{
   console.log('shops API:', storeShops)
   let allVenueX  = storeShops.response.venues;
-  var storeIds = [];
-  
-
-  for(let i = 0; i < 50; i++){
-    storeIds.push(allVenueX[i].id);
+   console.log(allVenueX);
+  for(let i = 0; i <= 40; i++){
     var storeImageFSQ = allVenueX[i].categories[0].icon.prefix + 64 + allVenueX[i].categories[0].icon.suffix;
     var defImgS       = localStorage.getItem('eto2');
     var storeCateg    = allVenueX[i].categories[0].name;
@@ -122,19 +145,23 @@ fetch('https://api.foursquare.com/v2/venues/search?ll=44.8099375,20.4494431&cate
     var  number       = allVenueX[i].contact.formattedPhone != undefined ?  ''+`${allVenueX[i].contact.formattedPhone}` : `<span id ='ifNoNumId'>No number info</span>`;
     var takeLatLon    =  'https://www.google.com/maps/search/?api=1&query=' + allVenueX[i].location.lat + ',' + allVenueX[i].location.lng
     document.getElementsByClassName('item44')[i].children[0].innerHTML =   ` <h3 title = ${allVenueX[i].categories[0].name} id = 'titleFSQ1'> ${allVenueX[i].name} &nbsp;&nbsp;&nbsp; <img id = 'imgFSQ1' src =${storeImageFSQ} /> <img id='defImg3' src=${defImgS}>  </h3>   <div id ='fsqTextDiv'> <p id ='adress4sq1'> <i title='Location of place' class="fa fa-map-signs"></i> <a id='latLonForSq' href=${takeLatLon} target=_blank title='Click to get there'>&nbsp;&nbsp;&nbsp;&nbsp;${storeAddress}</a> <br> <span title='Explanation, nearby streets' id='crossId'>&nbsp;&#9580; </span> &nbsp;&nbsp;&nbsp; ${strCross} <br> <i title='In which city?'  class="fa fa-address-card-o styleFaCard"></i> &nbsp;&nbsp; ${storeAddr1} ${storeAddr2}<br> <span id ='boldNumId'><span title='Phone number' id ='phone4SqId'>&#9742;</span> &nbsp;&nbsp;&nbsp; ${number}</span></p><img title='Internet adress' id ='style4ImgUrl' src ='https://image.flaticon.com/icons/svg/109/109476.svg' style="width:26px;height:28px;"/><p>&nbsp;<a id = 'sq4UrlTxt' href = ${storeUrl} target = "_blank">${prtUrl}...</a></p><p id='showOnSm'>Visit Site<p></div><p id ='categ4sqId'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Categories: <span id='sq4StoreCat'>${storeCateg}</span></p>`;
-    //console.log('pojavljuje li se ovde?')
     }
 
-    //localStorage.setItem('shopIds3', storeIds);
-    console.log('sta je problem ovde?')
-
  })
- .catch((err) => {
-   console.log('na 121 prosledjen error:', err)
- })
+ 
+ var idShops = localStorage.getItem('shopIds4').split(',');
+/*
+ for(let ii = 0; ii < idShops.length; ii++){
 
- console.log('u ovom radi! :)');
-
+  fetch('https://api.foursquare.com/v2/venues/' + idShops[ii]  + '?&client_id=SYQLZ1DXBSZYMCXG3QUGBBHDRM23YDDLO5SAZCALXMFUR3VS&client_secret=HHFBNGSRMFOUAFYQCTVMR1FK4HR4GZL5LO0T0BYGQVFUHSW0&v=20180130')
+  .then((detailedShops)=> {
+    return detailedShops.json();
+  })
+  .then((eachShop) => {
+    console.log("detaljno o shop svakom:", eachShop);
+  })
+ }
+*/
 }) //last then promise.
 
 
